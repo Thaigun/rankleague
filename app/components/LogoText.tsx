5e2a097ad1e40ace96093150a46d281a0df4f392
Antti Mäkihelmi <antti_rekisteroinnit@hotmail.com>
2025-05-21 22:39:37 +0300

    Logo text component and updated title.

---
+++ app/components/LogoText.tsx
@@ -0,0 +1,8 @@
+export function LogoText() {
+    return (
+        <h1 className='text-3xl'>
+            <span className='underline'>Rank</span>
+            <span>lig</span>
+        </h1>
+    );
+}
