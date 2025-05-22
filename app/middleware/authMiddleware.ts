79a54b5910b31ec78b4961f935df8c634752a66e
Antti Mäkihelmi <antti_rekisteroinnit@hotmail.com>
2025-05-22 22:04:15 +0300

    Implement authMiddleware

--- app/middleware/authMiddleware.ts
+++ app/middleware/authMiddleware.ts
@@ -1,17 +1,20 @@
 import { createMiddleware } from '@tanstack/react-start';
-import { getCookie, setCookie } from '@tanstack/react-start/server';
+import { getCookie } from '@tanstack/react-start/server';
+import { verify } from '../../src/crypto/jwt';
 
 export const authMiddleware = createMiddleware().server(async ({ next }) => {
-    const firstVisit = getCookie('firstVisit');
-    const visit = firstVisit ?? new Date().toISOString();
-    if (!firstVisit) {
-        setCookie('firstVisit', visit);
-    } else {
-        console.log('firstVisit', firstVisit);
+    const authCookie = getCookie('auth');
+    const secret = process.env.JWT_SECRET;
+
+    let verifiedToken: Record<string, any> | null = null;
+
+    if (authCookie && secret) {
+        verifiedToken = verify(authCookie, secret);
     }
+
     return next({
         context: {
-            visit,
+            auth: verifiedToken,
         },
     });
 });
