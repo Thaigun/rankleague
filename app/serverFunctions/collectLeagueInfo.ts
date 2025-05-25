514c1b7af31f3d1d16eb3769f483a29f908066dc
Antti Mäkihelmi <antti_rekisteroinnit@hotmail.com>
2025-05-25 15:31:11 +0300

    Implement league info function

--- app/serverFunctions/collectLeagueInfo.ts
+++ app/serverFunctions/collectLeagueInfo.ts
@@ -1,8 +1,46 @@
 import { createServerFn } from '@tanstack/react-start';
 import { leagueMembershipMiddleware } from '../middleware/leagueMembershipMiddleware';
+import { db } from '../../src/database/db';
 
 export const collectLeagueInfo = createServerFn()
     .middleware([leagueMembershipMiddleware])
-    .handler(async ({ context, data }) => {
-        return 'hello';
+    .handler(async ({ data }) => {
+        const leagueId = data.leagueId;
+        const [league, members, matches] = await Promise.all([
+            db
+                .selectFrom('league')
+                .where('id', '=', leagueId)
+                .select(['id', 'name', 'description', 'created_at'])
+                .executeTakeFirst(),
+            db
+                .selectFrom('member')
+                .where('league_id', '=', leagueId)
+                .select(['id', 'name', 'joined_at', 'is_admin'])
+                .execute(),
+            db
+                .selectFrom('match')
+                .innerJoin('member as m1', 'm1.id', 'match.member1_id')
+                .innerJoin('member as m2', 'm2.id', 'match.member2_id')
+                .where('m1.league_id', '=', leagueId)
+                .where('m2.league_id', '=', leagueId)
+                .select([
+                    'match.datetime as match_datetime',
+                    'match.id as match_id',
+                    'match.member1_score as member1_score',
+                    'match.member2_score as member2_score',
+                    'm1.id as member1_id',
+                    'm1.name as member1_name',
+                    'm2.id as member2_id',
+                    'm2.name as member2_name',
+                ])
+                .execute(),
+        ]);
+        if (!league) {
+            throw new Error('League not found');
+        }
+        return {
+            league,
+            members,
+            matches,
+        };
     });
